f9139f802dd508efbfe01ae6f50fc62e93715dc9
RCHD88 <richardraferguy@gmail.com>
2025-01-20 00:03:58 +0700

    added

--- pageObjects/LandingPage.py
+++ pageObjects/LandingPage.py
@@ -1,15 +1,28 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
 
 class LandingPage:
 
     def __init__(self, driver):
         self.driver = driver
 
+    # LOGIN
     login_button = (By.XPATH, "//a[normalize-space()='Sign In']")
     login_username = (By.XPATH, "//input[@placeholder='Type your email or phone number']")
     login_password = (By.XPATH, "//input[@placeholder='*********']")
     login_submit_button = (By.XPATH, "//button[normalize-space()='Sign In']")
 
+    # REGISTER
+    register_button = (By.XPATH, "//a[normalize-space()='Create an Account']")
+    register_code = (By.XPATH, "//select[@name='area_code']")
+    register_submit_button = (By.XPATH, "//button[normalize-space()='Sign Up']")
+    register_toast_message = (By.XPATH, "//div[@class='toast-message']")
+    
+    def getRegisterField(self, placeholder):
+        return (By.XPATH, f"//input[@id='{placeholder}']")
+
+    # LOGIN
     def click_login_button(self):
         self.driver.find_element(*self.login_button).click()
 
@@ -22,7 +35,40 @@ class LandingPage:
     def click_login_submit_button(self):
         self.driver.find_element(*self.login_submit_button).click()
     
+    # REGISTER
+    def click_register_button(self):
+        self.driver.find_element(*self.register_button).click()
 
+    def enter_code(self, code):
+        select = Select(self.driver.find_element(*self.register_code))
+        select.select_by_visible_text(code)
+
+    def enter_register_field(self, placeholder, value):
+        self.driver.find_element(*self.getRegisterField(placeholder)).send_keys(value)
+
+    def click_register_submit_button(self):
+        self.driver.find_element(*self.register_submit_button).click()
+
+    def getRegisterToastMessage(self):
+        return self.driver.find_element(*self.register_toast_message).text
+
+    def doLogin(self, username, password): 
+      time.sleep(2)    
+
+      # click go to login page
+      self.click_login_button()
+      time.sleep(0.5)
+
+      # enter username
+      self.enter_username(username)
+      time.sleep(0.5)
+      # enter password
+      self.enter_password(password)
+      time.sleep(0.5)
+      # click login button
+      self.click_login_submit_button()
+      time.sleep(2)
+    
 
 
 
---
+++ pageObjects/organization_related/OrganizationPage.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+class OrganizationPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    
+    
+
+
+    
+
+
+
